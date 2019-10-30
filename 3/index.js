958c61f627d7af7f9575ce07a568cc6137429593
ThongVM4 <vominhthong1603@gmail.com>
2019-10-30 20:35:58 +0700

    20191030-1

--- 3/index.js
+++ 3/index.js
@@ -14,8 +14,8 @@ app.get('/', function (req, res) {
 		name: 'Minh Phat',
 	});
 });
-app.get('/users', function (request, response) {
-	response.render('users/index', {
+app.get('/users', function (req, res) {
+	res.render('users/index', {
 		users: users
 	});
 });
@@ -24,6 +24,7 @@ app.get('/users/search', function (req, res) {
 	var q = req.query.q;
 	var matchedUsers = users.filter(function (user) {
 		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1; 
+		// return user.name.indexOf(q) !== -1; 
 	});
 
 	res.render('users/index', {
