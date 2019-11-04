84cea83e96cdbde33cef4d705f2c7b0b4dd91837
ThongVM4 <vominhthong1603@gmail.com>
2019-11-04 18:57:45 +0700

    20191104-2

--- 7/index.js
+++ 7/index.js
@@ -54,9 +54,9 @@ app.get('/users/create', function (req, res) {
 
 app.get('/users/:id', function (req, res) {
 	var id = req.params.id;
-	// console.log(id);
-	console.log(req.params);
+	// console.log(req.params);
 	var user = db.get('users').find({ id: id }).value();
+	console.log(user);
 	res.render('users/view', {
 		user: user
 	});
