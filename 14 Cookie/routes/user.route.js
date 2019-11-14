2b2a394058b33e586bdce35e8fd667ae4bf1e53f
ThongVM4 <vominhthong1603@gmail.com>
2019-11-14 12:41:24 +0700

    20191114-1

---
+++ 14 Cookie/routes/user.route.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate');
+
+router.get('/', controller.index);
+
+// function middleware1 (req, res, next) {
+// 	console.log('1');
+// 	next();
+// }
+// function middleware2 (req, res, next) {
+// 	console.log('2');
+// 	res.send('Hello');
+// }
+// router.get('/test', middleware1, middleware2);
+router.get('/cookie', function (req, res, next) {
+	res.cookie('user-id', 12345);
+	res.send('Hananim');
+});
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create', validate.postCreate, controller.postCreate);
+
+module.exports = router;
