957ff8b9fa3f5cdbdca9d30042637ea68b9eb890
thongvm316 <vominhthong1603@gmail.com>
2019-12-04 14:51:25 +0700

    20191204

---
+++ 27 REST API/db.js
@@ -0,0 +1,14 @@
+// lowdb
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [], sessions: [], transfers: [] })
+  .write()
+
+
+
+
+ module.exports = db;
